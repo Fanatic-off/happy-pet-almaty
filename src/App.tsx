eb9d6427151d7b5dee86e2171beedb3218830f64
Евгений Кулинич <kulinich.eugene.2412@gmail.com>
2025-09-20 16:09:23 +0500

    init HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { ROUTES } from "./store/routes";
+import { HomePage } from "./pages/HomePage/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <Routes>
+          <Route index path={ROUTES.HOME} element={<HomePage />} />
+        </Routes>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import "./HomePage.scss";
+
+export const HomePage = () => {
+  return (
+    <div id="home-page-id">
+      <div>HomePage</div>
+    </div>
+  );
+};
