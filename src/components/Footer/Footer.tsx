9f11ec7a43a8938fa26a98f24c817d3bed5eea2e
Евгений Кулинич <kulinich.eugene.2412@gmail.com>
2025-09-21 15:09:18 +0500

    add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,37 @@
 import "./Footer.scss";
 import puppy from "./assets/puppy.svg";
-import phones from "./assets/phone.svg";
+import phone from "./assets/phone.svg";
 import marker from "./assets/marker.svg";
 
 export const Footer = () => {
   return (
     <div id="footer-id">
-      <div className="questions">questions</div>
-      <div className="location">questions</div>
+      <div className="info">
+        <div className="questions">
+          <p className="title">For questions and suggestions</p>
+
+          <div className="row">
+            <div>
+              <img src={phone} alt="phone" />
+            </div>
+
+            <div>87472122619</div>
+          </div>
+        </div>
+
+        <div className="location">
+          <p className="title">We are waiting for your visit</p>
+
+          <div className="row">
+            <div>
+              <img src={marker} alt="marker" />
+            </div>
+
+            <div>Almaty, Kazakhstan</div>
+          </div>
+        </div>
+      </div>
+
       <div className="puppy">
         <img className="puppy-img" src={puppy} alt="puppy" />
       </div>
