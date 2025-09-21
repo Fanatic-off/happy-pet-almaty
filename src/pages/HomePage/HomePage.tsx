53c9c7bcf3002058018ce0fdfad04f41d46492bd
Евгений Кулинич <kulinich.eugene.2412@gmail.com>
2025-09-21 21:54:49 +0500

    add photo section layout

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
 import { AboutUs } from "./components/AboutUs/AboutUs";
 import { HeroSection } from "./components/HeroSection/HeroSection";
+import { PhotoSection } from "./components/PhotoSection/PhotoSection";
 
 export const HomePage = () => {
   return (
     <>
       <HeroSection />
       <AboutUs />
+      <PhotoSection />
     </>
   );
 };
---
+++ src/pages/HomePage/components/PhotoSection/PhotoSection.tsx
@@ -0,0 +1,41 @@
+import "./PhotoSection.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+var settings = {
+  className: "center",
+  centerMode: true,
+  infinite: true,
+  centerPadding: "60px",
+  slidesToShow: 3,
+  speed: 500,
+};
+
+export const PhotoSection = () => {
+  return (
+    <div id="photo-section">
+      <h1>Наши фотографии</h1>
+
+      <div className="slider">
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3></h3>
+          </div>
+        </Slider>
+      </div>
+    </div>
+  );
+};
