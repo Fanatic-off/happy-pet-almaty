202fbe1d1f44cdfde8881b0308384491f6df49ce
Евгений Кулинич <kulinich.eugene.2412@gmail.com>
2025-09-21 16:19:12 +0500

    add AboutUs

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,5 @@
 import { AboutUs } from "./components/AboutUs/AboutUs";
 import { HeroSection } from "./components/HeroSection/HeroSection";
-import "./HomePage.scss";
 
 export const HomePage = () => {
   return (
